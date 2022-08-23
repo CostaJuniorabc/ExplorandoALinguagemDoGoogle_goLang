f02e796631878f293458e2bc1b3938a6b025445d
Antonio Carlos da Costa Junior <anronio.juniorabc@yahoo.com.br>
2022-08-22 22:53:17 -0300

    Update Project

---
+++ fundamentos/primoroEx/primeiro.go
@@ -0,0 +1,18 @@
+//programas execultáveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em GO são organizados em pacotes
+e para usá-los é necessario declarar um ou vários imports
+*/
+import (
+	"fmt"
+)
+
+//A porta de entrada de um programa GO é  a função main
+func main() {
+
+	fmt.Printf("Primeiro")
+	fmt.Println("Programa em")
+	fmt.Println("GoLang")
+}
